aab01320b27dd52d79ca3c8847b56a2c540e7d00
Роман Ширяев <shiryaev@elewise.com>
2018-01-22 19:00:14 +0400

    Изменено тело функции в соответствии с требуемой логикой Скорректирован README.md Исправлено тестовое наполнение

--- get_prc_summ.sql
+++ get_prc_summ.sql
@@ -1,77 +1,113 @@
--- краеугольные кейсы:
--- не было оборотов за период
--- обороты были в граничные дни
 CREATE OR REPLACE FUNCTION get_prc_summ
 (
-  p_account_id IN NUMBER
- ,p_start_date IN DATE
- ,p_end_date   IN DATE
- ,p_prc_rate   IN NUMBER
+  p_account_code IN VARCHAR2 -- Код счета
+ ,p_start_date   IN DATE -- Дата начала расчетного периода (будет округлена до дня)
+ ,p_end_date     IN DATE -- Дата окончания расчетного периода (будет округлена до дня)
+ ,p_prc_rate     IN NUMBER -- Процентная ставка в процентах, т.е. если ставка равна 10%, то передавать 10
 ) RETURN NUMBER IS
-  l_result     NUMBER;
+  l_result     NUMBER := 0;
+  l_year_days  NUMBER;
   l_start_date DATE := trunc(p_start_date
-                            ,'DD') - 1;
+                            ,'DD');
   l_end_date   DATE := trunc(p_end_date
                             ,'DD');
-BEGIN
-  WITH p AS
-   (SELECT l_start_date start_date
-          ,l_end_date   end_date
-      FROM dual),
-  periods AS
-   (SELECT lag(trunc(r.accounting_date
-                    ,'DD')
-              ,1
-              ,p.start_date) over(ORDER BY trunc(r.accounting_date, 'DD') ASC) + 1 accounting_day_start
-          ,trunc(r.accounting_date
-                ,'DD') accounting_day_end
-          ,(SELECT coalesce(SUM(r1.accounted_sum)
-                           ,0)
-              FROM ow_turnovers r1
-             WHERE r1.account_id = r.account_id
-               AND r1.accounting_date <= trunc(r.accounting_date
-                                              ,'DD')) amount
-      FROM ow_turnovers r
-      JOIN p
-        ON trunc(r.accounting_date
-                ,'DD') BETWEEN p.start_date AND p.end_date - 1
-     WHERE 1 = 1
-       AND r.account_id = p_account_id
-     GROUP BY trunc(r.accounting_date
-                   ,'DD')
-             ,r.account_id
-             ,p.start_date
-    
-    UNION ALL
-    
-    SELECT t.accounting_day_start
-          ,t.accounting_day_end
-          ,(SELECT coalesce(SUM(r1.accounted_sum)
-                           ,0)
-              FROM ow_turnovers r1
-             WHERE r1.account_id = t.account_id
-               AND r1.accounting_date <= t.accounting_day_end) amount
-    
-      FROM (SELECT nvl(MAX(trunc(r.accounting_date
-                                ,'DD'))
-                       keep(dense_rank FIRST ORDER BY trunc(r.accounting_date
-                                 ,'DD') DESC)
-                      ,MAX(p.start_date)) + 1 accounting_day_start
-                  ,MAX(p.end_date) accounting_day_end
-                  ,MAX(r.account_id) account_id
+  l_prev_date  DATE := l_start_date;
+  l_amount     NUMBER;
+  l_account_id NUMBER;
+
+  CURSOR dates
+  (
+    c_start_date IN DATE
+   ,c_end_date   IN DATE
+   ,c_account_id IN NUMBER
+  ) IS
+    SELECT accounting_day_end
+          ,amount
+      FROM (
+            -- 1. Собираем строчки с датами, когда происходила смена остатка на счете
+            SELECT trunc(r.accounting_date
+                         ,'DD') accounting_day_end
+                   ,(SELECT coalesce(SUM(r1.accounted_sum)
+                                    ,0)
+                       FROM ow_turnovers r1
+                      WHERE r1.account_id = c_account_id
+                        AND r1.accounting_date <=
+                            trunc(r.accounting_date
+                                 ,'DD')) amount
               FROM ow_turnovers r
-              JOIN p
-                ON trunc(r.accounting_date
-                        ,'DD') BETWEEN p.start_date AND p.end_date - 1
+            
              WHERE 1 = 1
-               AND r.account_id = p_account_id) t
-    
-    )
-  SELECT coalesce(SUM((r.accounting_day_end - r.accounting_day_start + 1) *
-                      p_prc_rate * r.amount / 365)
+               AND r.account_id = c_account_id
+               AND trunc(r.accounting_date
+                        ,'DD') BETWEEN c_start_date AND c_end_date - 1
+             GROUP BY trunc(r.accounting_date
+                            ,'DD')
+            
+            UNION ALL
+            -- 2. Добавляем строчки со стыками годов (последний день года)
+            SELECT accounting_day_end accounting_day_end
+                   ,(SELECT coalesce(SUM(r1.accounted_sum)
+                                    ,0)
+                       FROM ow_turnovers r1
+                      WHERE r1.account_id = c_account_id
+                        AND r1.accounting_date <=
+                            trunc(r.accounting_day_end
+                                 ,'DD')) amount
+              FROM (SELECT add_months(trunc(c_start_date
+                                            ,'YYYY') - 1
+                                      ,12 * (LEVEL - 1)) accounting_day_end
+                       FROM dual
+                     CONNECT BY LEVEL <=
+                                to_number(to_char(c_end_date
+                                                 ,'YYYY')) -
+                                to_number(to_char(c_start_date
+                                                 ,'YYYY')) + 1) r)
+     WHERE accounting_day_end > c_start_date -- оставляем только те изменения, которые произошли после даты начала запрашиваемого периода
+     ORDER BY 1 ASC; -- сортируем строчки в порядке даты учета
+BEGIN
+  -- определяем ID счета
+  SELECT account_id
+    INTO l_account_id
+    FROM ow_accounts
+   WHERE account_code = p_account_code;
+
+  -- пробегаемся по собранным записям
+  FOR line IN dates(l_start_date - 1
+                   ,l_end_date
+                   ,l_account_id)
+  LOOP
+    -- подсчитываем число дней в году, к которому принадлежит период
+    l_year_days := add_months(trunc(line.accounting_day_end
+                                   ,'YYYY')
+                             ,12) - trunc(line.accounting_day_end
+                                         ,'YYYY');
+    -- рассчитываем результат
+    l_result := l_result +
+                round((line.accounting_day_end - l_prev_date + 1) *
+                      p_prc_rate * line.amount / 100 / l_year_days
+                     ,2);
+  
+    -- сохраняем текущую конечную данную как предыдущую для следующей итерации курсора
+    l_prev_date := line.accounting_day_end + 1;
+  END LOOP;
+
+  -- повторяем действия для конечной даты запрашиваемого периода
+  l_year_days := add_months(trunc(l_end_date
+                                 ,'YYYY')
+                           ,12) - trunc(l_end_date
+                                       ,'YYYY');
+
+  SELECT coalesce(SUM(r1.accounted_sum)
                  ,0)
-    INTO l_result
-    FROM periods r;
+    INTO l_amount
+    FROM ow_turnovers r1
+   WHERE r1.account_id = l_account_id
+     AND r1.accounting_date <= trunc(l_end_date
+                                    ,'DD');
+
+  l_result := l_result + round((l_end_date - l_prev_date + 1) * p_prc_rate *
+                               l_amount / 100 / l_year_days
+                              ,2);
 
   RETURN l_result;
 END get_prc_summ;
--- test_fill.sql
+++ test_fill.sql
@@ -36,194 +36,226 @@ INSERT INTO ow_turnovers
   ,accounted_sum
   ,accounting_date
   ,comments)
-  SELECT 45
+  SELECT (select account_id from ow_accounts where account_code = '92')
         ,139.39
-        ,DATE '2017-10-22'
+        ,to_date('22.10.2017 10:48:46'
+                ,'DD.MM.YYYY HH24:MI:SS')
         ,'Первичное пополнение счета'
     FROM dual
   UNION ALL
-  SELECT 45
+  SELECT (select account_id from ow_accounts where account_code = '92')
         ,841.23
-        ,DATE '2017-11-19'
+        ,to_date('19.11.2017 06:44:17'
+                ,'DD.MM.YYYY HH24:MI:SS')
         ,'Перенос со счета 90 на счет 92'
     FROM dual
   UNION ALL
-  SELECT 45
+  SELECT (select account_id from ow_accounts where account_code = '92')
         ,-304.95
-        ,DATE '2017-11-02'
+        ,to_date('02.11.2017 04:14:38'
+                ,'DD.MM.YYYY HH24:MI:SS')
         ,'Перенос со счета 92 на счет 97'
     FROM dual
   UNION ALL
-  SELECT 45
+  SELECT (select account_id from ow_accounts where account_code = '92')
         ,-814.95
-        ,DATE '2018-01-10'
+        ,to_date('10.01.2018 19:03:28'
+                ,'DD.MM.YYYY HH24:MI:SS')
         ,'Списание наличных средств'
     FROM dual
   UNION ALL
-  SELECT 46
+  SELECT (select account_id from ow_accounts where account_code = '95')
         ,273.25
-        ,DATE '2017-10-16'
+        ,to_date('16.10.2017 14:48:24'
+                ,'DD.MM.YYYY HH24:MI:SS')
         ,'Первичное пополнение счета'
     FROM dual
   UNION ALL
-  SELECT 46
+  SELECT (select account_id from ow_accounts where account_code = '95')
         ,863.26
-        ,DATE '2017-11-25'
+        ,to_date('25.11.2017 12:13:50'
+                ,'DD.MM.YYYY HH24:MI:SS')
         ,'Перенос со счета 97 на счет 95'
     FROM dual
   UNION ALL
-  SELECT 46
+  SELECT (select account_id from ow_accounts where account_code = '95')
         ,-557.19
-        ,DATE '2018-01-13'
+        ,to_date('13.01.2018 15:15:18'
+                ,'DD.MM.YYYY HH24:MI:SS')
         ,'Списание наличных средств'
     FROM dual
   UNION ALL
-  SELECT 47
+  SELECT (select account_id from ow_accounts where account_code = '97')
         ,304.95
-        ,DATE '2017-11-02'
+        ,to_date('02.11.2017 04:14:38'
+                ,'DD.MM.YYYY HH24:MI:SS')
         ,'Перенос со счета 92 на счет 97'
     FROM dual
   UNION ALL
-  SELECT 47
+  SELECT (select account_id from ow_accounts where account_code = '97')
         ,406.21
-        ,DATE '2017-12-27'
+        ,to_date('27.12.2017 20:14:28'
+                ,'DD.MM.YYYY HH24:MI:SS')
         ,'Перенос со счета 98 на счет 97'
     FROM dual
   UNION ALL
-  SELECT 47
+  SELECT (select account_id from ow_accounts where account_code = '97')
         ,959.32
-        ,DATE '2017-10-26'
+        ,to_date('26.10.2017 04:05:51'
+                ,'DD.MM.YYYY HH24:MI:SS')
         ,'Первичное пополнение счета'
     FROM dual
   UNION ALL
-  SELECT 47
+  SELECT (select account_id from ow_accounts where account_code = '97')
         ,-390.91
-        ,DATE '2018-01-19'
+        ,to_date('19.01.2018 13:54:28'
+                ,'DD.MM.YYYY HH24:MI:SS')
         ,'Списание наличных средств'
     FROM dual
   UNION ALL
-  SELECT 47
+  SELECT (select account_id from ow_accounts where account_code = '97')
         ,-632.42
-        ,DATE '2017-12-13'
+        ,to_date('13.12.2017 17:48:10'
+                ,'DD.MM.YYYY HH24:MI:SS')
         ,'Перенос со счета 97 на счет 90'
     FROM dual
   UNION ALL
-  SELECT 47
+  SELECT (select account_id from ow_accounts where account_code = '97')
         ,-835.08
-        ,DATE '2018-01-06'
+        ,to_date('06.01.2018 05:06:21'
+                ,'DD.MM.YYYY HH24:MI:SS')
         ,'Перенос со счета 97 на счет 90'
     FROM dual
   UNION ALL
-  SELECT 47
+  SELECT (select account_id from ow_accounts where account_code = '97')
         ,-863.26
-        ,DATE '2017-11-25'
+        ,to_date('25.11.2017 12:13:50'
+                ,'DD.MM.YYYY HH24:MI:SS')
         ,'Перенос со счета 97 на счет 95'
     FROM dual
   UNION ALL
-  SELECT 48
+  SELECT (select account_id from ow_accounts where account_code = '91')
         ,273.42
-        ,DATE '2017-10-15'
+        ,to_date('15.10.2017 20:20:45'
+                ,'DD.MM.YYYY HH24:MI:SS')
         ,'Первичное пополнение счета'
     FROM dual
   UNION ALL
-  SELECT 48
+  SELECT (select account_id from ow_accounts where account_code = '91')
         ,-565.28
-        ,DATE '2018-01-05'
+        ,to_date('05.01.2018 23:24:11'
+                ,'DD.MM.YYYY HH24:MI:SS')
         ,'Перенос со счета 91 на счет 98'
     FROM dual
   UNION ALL
-  SELECT 48
+  SELECT (select account_id from ow_accounts where account_code = '91')
         ,-632.76
-        ,DATE '2018-01-11'
+        ,to_date('11.01.2018 22:24:57'
+                ,'DD.MM.YYYY HH24:MI:SS')
         ,'Списание наличных средств'
     FROM dual
   UNION ALL
-  SELECT 49
+  SELECT (select account_id from ow_accounts where account_code = '90')
         ,415.19
-        ,DATE '2017-10-22'
+        ,to_date('22.10.2017 22:19:43'
+                ,'DD.MM.YYYY HH24:MI:SS')
         ,'Первичное пополнение счета'
     FROM dual
   UNION ALL
-  SELECT 49
+  SELECT (select account_id from ow_accounts where account_code = '90')
         ,590.66
-        ,DATE '2017-12-31'
+        ,to_date('31.12.2017 08:02:56'
+                ,'DD.MM.YYYY HH24:MI:SS')
         ,'Перенос со счета 98 на счет 90'
     FROM dual
   UNION ALL
-  SELECT 49
+  SELECT (select account_id from ow_accounts where account_code = '90')
         ,632.42
-        ,DATE '2017-12-13'
+        ,to_date('13.12.2017 17:48:10'
+                ,'DD.MM.YYYY HH24:MI:SS')
         ,'Перенос со счета 97 на счет 90'
     FROM dual
   UNION ALL
-  SELECT 49
+  SELECT (select account_id from ow_accounts where account_code = '90')
         ,677.95
-        ,DATE '2017-11-13'
+        ,to_date('13.11.2017 22:06:14'
+                ,'DD.MM.YYYY HH24:MI:SS')
         ,'Перенос со счета 98 на счет 90'
     FROM dual
   UNION ALL
-  SELECT 49
+  SELECT (select account_id from ow_accounts where account_code = '90')
         ,835.08
-        ,DATE '2018-01-06'
+        ,to_date('06.01.2018 05:06:21'
+                ,'DD.MM.YYYY HH24:MI:SS')
         ,'Перенос со счета 97 на счет 90'
     FROM dual
   UNION ALL
-  SELECT 49
+  SELECT (select account_id from ow_accounts where account_code = '90')
         ,-215.19
-        ,DATE '2018-01-13'
+        ,to_date('13.01.2018 22:32:50'
+                ,'DD.MM.YYYY HH24:MI:SS')
         ,'Списание наличных средств'
     FROM dual
   UNION ALL
-  SELECT 49
+  SELECT (select account_id from ow_accounts where account_code = '90')
         ,-525.89
-        ,DATE '2018-01-08'
+        ,to_date('08.01.2018 16:00:07'
+                ,'DD.MM.YYYY HH24:MI:SS')
         ,'Перенос со счета 90 на счет 98'
     FROM dual
   UNION ALL
-  SELECT 49
+  SELECT (select account_id from ow_accounts where account_code = '90')
         ,-841.23
-        ,DATE '2017-11-19'
+        ,to_date('19.11.2017 06:44:17'
+                ,'DD.MM.YYYY HH24:MI:SS')
         ,'Перенос со счета 90 на счет 92'
     FROM dual
   UNION ALL
-  SELECT 50
+  SELECT (select account_id from ow_accounts where account_code = '98')
         ,-57.13
-        ,DATE '2018-01-12'
+        ,to_date('12.01.2018 11:19:10'
+                ,'DD.MM.YYYY HH24:MI:SS')
         ,'Списание наличных средств'
     FROM dual
   UNION ALL
-  SELECT 50
+  SELECT (select account_id from ow_accounts where account_code = '98')
         ,525.89
-        ,DATE '2018-01-08'
+        ,to_date('08.01.2018 16:00:07'
+                ,'DD.MM.YYYY HH24:MI:SS')
         ,'Перенос со счета 90 на счет 98'
     FROM dual
   UNION ALL
-  SELECT 50
+  SELECT (select account_id from ow_accounts where account_code = '98')
         ,565.28
-        ,DATE '2018-01-05'
+        ,to_date('05.01.2018 23:24:11'
+                ,'DD.MM.YYYY HH24:MI:SS')
         ,'Перенос со счета 91 на счет 98'
     FROM dual
   UNION ALL
-  SELECT 50
+  SELECT (select account_id from ow_accounts where account_code = '98')
         ,942.13
-        ,DATE '2017-10-25'
+        ,to_date('25.10.2017 11:53:03'
+                ,'DD.MM.YYYY HH24:MI:SS')
         ,'Первичное пополнение счета'
     FROM dual
   UNION ALL
-  SELECT 50
+  SELECT (select account_id from ow_accounts where account_code = '98')
         ,-406.21
-        ,DATE '2017-12-27'
+        ,to_date('27.12.2017 20:14:28'
+                ,'DD.MM.YYYY HH24:MI:SS')
         ,'Перенос со счета 98 на счет 97'
     FROM dual
   UNION ALL
-  SELECT 50
+  SELECT (select account_id from ow_accounts where account_code = '98')
         ,-590.66
-        ,DATE '2017-12-31'
+        ,to_date('31.12.2017 08:02:56'
+                ,'DD.MM.YYYY HH24:MI:SS')
         ,'Перенос со счета 98 на счет 90'
     FROM dual
   UNION ALL
-  SELECT 50
+  SELECT (select account_id from ow_accounts where account_code = '98')
         ,-677.95
-        ,DATE '2017-11-13'
+        ,to_date('13.11.2017 22:06:14'
+                ,'DD.MM.YYYY HH24:MI:SS')
         ,'Перенос со счета 98 на счет 90'
     FROM dual;
